5b2ddd291ea761325254123a3d2188f289a6606b
json <json.panganiban@gmail.com>
2021-05-11 20:57:30 +1000

    updated topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
+import "../../global.scss"
 import "./topbar.scss"
+import EmailSharpIcon from '@material-ui/icons/EmailSharp';
+import PersonSharpIcon from '@material-ui/icons/PersonSharp';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
-      
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+
+        <div className="left">
+          <a href="#intro" className="logo">jsoncp.</a>
+          <div className="itemContainter">
+            <PersonSharpIcon className="icon" />
+            <span>+61 446111211</span>
+          </div>
+          <div className="itemContainer">
+            <EmailSharpIcon className="icon" />
+            <span>jsoncp@protonmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,7 @@ import "./works.scss"
 
 export default function Wworks() {
   return (
-    <div className="works">
+    <div className="works" id="works">
       Works
     </div>
   )
